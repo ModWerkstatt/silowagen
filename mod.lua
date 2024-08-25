a2740dd57d6e1eb11f40966a787280f73318e3c7
Florian Thiel <flth@flth.de>
2024-08-25 14:05:24 +0200

    Initial

---
+++ mod.lua
@@ -0,0 +1,28 @@
+﻿function data()
+return {
+	info = {
+		minorVersion = 0,
+		severityAdd = "NONE",
+		severityRemove = "WARNING",
+		name = _("mod_name"),
+		description = _("mod_desc"),
+		authors = {
+		    {
+		        name = "ModWerkstatt",
+		        role = "CREATOR",
+		    },
+		},
+		tags = { "DB", "Waggon", "Germany", "Silo"," Europe",},
+		tfnetId = { },
+		minGameVersion = 0,
+		dependencies = { },
+		url = { "" },
+	},
+	options = {
+	},
+	
+	runFn = function (settings)
+	
+	end
+	}
+end
---
+++ strings.lua
@@ -0,0 +1,87 @@
+function data()
+return {
+	en = {
+
+		["mod_name"] = "Silo waggon of DB",
+		["mod_desc"] = "Before the development of modern silo wagons, powdered goods (e.g. cement, quartz sand, phosphate, fly ash, flour and sugar) had to be transported either packed in bags in boxcars or in wagons with a hinged lid. Both types had to be loaded and unloaded by hand. On an experimental basis, the DB had four-axle tank wagons of the American wartime design, which were still in good condition, converted to type KKd 49 (Uac945) silo wagons. These could be loaded and unloaded very efficiently with compressed air. After these had proven themselves, new wagons were developed. After a pre-series in 1953, around 1200 vehicles were delivered between 1955 and 1964, which the DB classified as Kds 54 (Ucs908) in its freight car fleet.",
+		
+        ["kds54_menu"] = "Silo waggon Kds 54",
+        ["kds56_menu"] = "Silo waggon Kds 56",
+        ["ucs946_menu"] = "Silo waggon Kkd 59",
+        
+        ["with_brake"] = "with handbrake",
+		
+        ["silo_54_1_name"] = "Kds 54 DB",
+        ["silo_54_2_name"] = "Kds 54 Quarz Werke",
+        ["silo_54_3_name"] = "Kds 54 Dyckerhoff",
+        ["silo_54_4_name"] = "Ucs 908 DB AG",
+        ["silo_54_5_name"] = "Kds 54 WLE",
+        ["silo_54_6_name"] = "Kds 54 Clubkraft",
+        ["silo_54_7_name"] = "Kds 54 akw",
+        ["silo_54_8_name"] = "Kds 54 Franken Zucker",
+        ["silo_54_9_name"] = "Kds 54 Brandt",
+        ["silo_54_10_name"] = "Ucs 908 VTG",
+        ["silo_54_11_name"] = "Kds 54 Umkehr Bohnen",
+		["silo_54_desc"] = "After a test series of 10 non s-capable Kd 54s were put into operation in 1953, the DB procured a total of 50 silo wagons in 1954/55, which were referred to as Kds 54s. The wagons with double shackle running gear have containers with a total volume of 25 m³, 15 of them with hand brakes. 101 wagons built between 1954 and 1956 have containers with a capacity of 27 m³ and double jacks, 15 of which have a hand brake on the gangway.",
+		
+        ["silo_56_1_name"] = "Kds 56 DB",
+        ["silo_56_2_name"] = "Ucs 909 DB AG",
+        ["silo_56_3_name"] = "Kds 56 Clubkraft",
+        ["silo_56_4_name"] = "Kds 56 Quarz Werke",
+        ["silo_56_5_name"] = "Kds 56 Eva",
+        ["silo_56_6_name"] = "Kds 56 Dyckerhoff",
+        ["silo_56_7_name"] = "Kds 56 Franken Zucker",
+        ["silo_56_8_name"] = "Kds 56 Brandt",
+        ["silo_56_9_name"] = "Ucs 909 Brandt",
+        ["silo_56_10_name"] = "Ucs 909 VTG",
+        ["silo_56_11_name"] = "Ucs 909 Heidelberger",		
+		["silo_56_desc"] = "From 1955, the Deutsche Bundesbahn put another type of two-axle dust container wagon into service under the designation Kds 56. The base corresponds to the Kds 54, the volume of the container is increased to 2 × 17 m³; this means that the wagons are no longer RIV-capable.",
+		
+        ["ucs946_1_name"] = "KKd 59 DB",
+		["ucs946_2_name"] = "KKd 59 DB (blackened)",
+		["ucs946_desc"] = "In 1950, the Deutsche Bundesbahn had a four-axle container wagon developed for the transport of alumina and built in a trial series of three units, which were designated the KKd 55. These wagons have four containers, each with a capacity of 17 m³. The emptying devices correspond to the Ucs wagons, the filling openings are closed with flaps, the loosening floors are made of ceramic.", 
+		
+	},
+	de = {
+
+		["mod_name"] = "Silowagen DB",
+		["mod_desc"] = "Vor der Entwicklung moderner Silowagen mussten staubförmige Güter (z. B. Zement, Quarzsand, Phosphat, Flugasche sowie Mehl und Zucker) entweder in Säcke verpackt in gedeckten Güterwagen oder in Klappdeckelwagen befördert werden. Bei beiden Arten musste von Hand be- und entladen werden. Versuchsweise ließ die DB vierachsige Kesselwagen der amerikanischen Kriegsbauart, die noch recht gut erhalten waren, zu Silowagen KKd 49 (Uac945) umbauen. Diese konnten sehr rationell mit Druckluft be- und entladen werden. Nachdem sich diese bewährt hatten, wurden neue Wagen entwickelt. Nach einer Vorserie im Jahr 1953 wurden zwischen 1955 und 1964 etwa 1200 Fahrzeuge abgeliefert, die die DB als Kds 54 (Ucs908) in ihren Güterwagenpark einreihte.",
+		
+		["kds54_menu"] = "Silowagen Kds 54",
+		["kds56_menu"] = "Silowagen Kds 56",
+        ["ucs946_menu"] = "Silowagen Kkd 59",
+
+        ["with_brake"] = " mit Handbremse",		
+
+        ["silo_54_1_name"] = "Kds 54 DB",
+        ["silo_54_2_name"] = "Kds 54 Quarz Werke",
+        ["silo_54_3_name"] = "Kds 54 Dyckerhoff",
+        ["silo_54_4_name"] = "Ucs 908 DB AG",
+        ["silo_54_5_name"] = "Kds 54 WLE",
+        ["silo_54_6_name"] = "Kds 54 Clubkraft",
+        ["silo_54_7_name"] = "Kds 54 akw",
+        ["silo_54_8_name"] = "Kds 54 Franken Zucker",
+        ["silo_54_9_name"] = "Kds 54 Brandt",
+        ["silo_54_10_name"] = "Ucs 908 VTG",
+        ["silo_54_11_name"] = "Kds 54 Umkehr Bohnen",
+		["silo_54_desc"] = "Nachdem 1953 eine Probeserie von 10 Kd 54 dem Betrieb übergeben wurden, beschaffte die DB in den Jahren 1954/55 insgesamt 50 Silowagen, die als Kds 54 bezeichnet werden. Die Wagen erhalten Behälter mit einem Volumen von insgesamt 25 m³, 15 davon mit Handbremse. 101 Wagen der Baujahre 1954-56 verfügen über Behälter mit 27 m³ Fassungsvermögen, 15 davon mit Handbremse am Übergangssteg.",
+		
+        ["silo_56_1_name"] = "Kds 56 DB",
+        ["silo_56_2_name"] = "Ucs 909 DB AG",
+        ["silo_56_3_name"] = "Kds 56 Clubkraft",
+        ["silo_56_4_name"] = "Kds 56 Quarz Werke",
+        ["silo_56_5_name"] = "Kds 56 Eva",
+        ["silo_56_6_name"] = "Kds 56 Dyckerhoff",
+        ["silo_56_7_name"] = "Kds 56 Franken Zucker",
+        ["silo_56_8_name"] = "Kds 56 Brandt",
+        ["silo_56_9_name"] = "Ucs 909 Brandt",
+        ["silo_56_10_name"] = "Ucs 909 VTG",
+        ["silo_56_11_name"] = "Ucs 909 Heidelberger",		
+		["silo_56_desc"] = "Unter der Bezeichnung Kds 56 stellt die Deutsche Bundesbahn ab 1955 eine weitere Bauart zweiachsiger Staubbehälterwagen in Dienst. Das Untergestell entspricht dem Kds 54, das Volumen der Behälter wird auf 2× 17 m³ vergrößert; damit sind die Wagen nicht mehr RIV-fähig.",
+		
+        ["ucs946_1_name"] = "KKd 59 DB",
+		["ucs946_2_name"] = "KKd 59 DB (verschwärzt)",
+		["ucs946_desc"] = "1950 lässt die Deutsche Bundesbahn für den Transport von Tonerde einen vierachsigen Behälterwagen entwickeln und in einer Probeserie von drei Stück bauen, die als KKd 55 bezeichnet werden. Diese Wagen haben vier Behälter mit jeweils 17 m³ Fassungsvermögen. Die Entleerungseinrichtungen entsprechen den Ucs-Wagen, die Füllöffnungen werden mit Klappen verschlossen, die Auflockerungsböden bestehen aus Keramik.", 
+	},
+}
+end
